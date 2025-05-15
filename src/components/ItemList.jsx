bff9295c03a420b7830d94415da8d000822d35a2
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-05-15 16:53:10 +0330

    add localstorage and fix it

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,7 +4,9 @@ import { FaTrash } from "react-icons/fa";
 import { MdDone } from "react-icons/md";
 import { RxCross2 } from "react-icons/rx";
 
-const ItemList = ({data:{id,todo},deleteHandler,editHandler}) => {
+const ItemList = ({data:{id,todo,date},deleteHandler,editHandler}) => {
+    console.log(date);
+ 
     const [newTodo,setNewTodo]=useState(todo);
     const [isEditing,setIsEditing]=useState(false);
     const [isDone,setIsDone]=useState(true);
@@ -15,6 +17,7 @@ const ItemList = ({data:{id,todo},deleteHandler,editHandler}) => {
            <>
             <input className="flex-1 border-none outline-none placeholder:tex-[3px]" type="text" value={newTodo} onChange={(e)=>setNewTodo(e.target.value)} placeholder="you can change now" maxLength="40" />
            {alert&& <p className="mr-5 text-red-600">{alert}</p>}
+          
            </>
           ):<p className={isDone ? "flex-1" : "flex-1 opacity-55 text-gray-400 line-through"}>{todo}</p>}
 
@@ -33,7 +36,7 @@ const ItemList = ({data:{id,todo},deleteHandler,editHandler}) => {
           }} className="mr-2 cursor-pointer hover:scale-90 transition duration-150 ease-in-out">edit</button>)}
 
 
-
+        
             <button className="mr-2 cursor-pointer hover:scale-90 transition duration-150 ease-in-out" onClick={()=>{
                 if(!isEditing){
                     setIsDone(false)
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,71 +1,81 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 } from "uuid";
-
 
 import AddTodoInput from "./AddTodoInput";
 import TodoList from "./TodoList";
 import Modal from "./Modal";
 
-
-
-
 const Todo = () => {
-    const [todo,setTodo]=useState({
-        id:"",
-        todo:""
-    });
+  const [todo, setTodo] = useState({
+    id: "",
+    todo: "",
+    date:"",
+  });
 
-    const [todoList,setTodoList]=useState([]);
-    const [modal,setModal]=useState(false);
-    
-    const todoHandler=(event)=>{
-    const {value,name}=event.target;
-    setTodo((prevTodo)=>({...prevTodo,[name]:value.toLowerCase()}));
-   };
+  const [todoList, setTodoList] = useState(() => {
+    const stored = localStorage.getItem("todos");
+    return stored ? JSON.parse(stored) : [];
+  });
+  const [modal, setModal] = useState(false);
 
-   const addtodoHandler=()=>{
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
 
-    if(!todo.todo.trim()){
-        setModal(true);
-        return
+  const todoHandler = (event) => {
+    const { value, name } = event.target;
+    setTodo((prevTodo) => ({ ...prevTodo, [name]: value.toLowerCase() }));
+  };
+
+  const addtodoHandler = () => {
+    if (!todo.todo.trim()) {
+      setModal(true);
+      return;
     }
 
-    const newTodo={...todo,id:v4()};
-    setTodoList((prevTodoList)=>([...prevTodoList,newTodo]));
+    const newTodo = { ...todo, id: v4(),date:new Date().toLocaleString("fa-IR")};
+    console.log(newTodo);
+    setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
     setTodo({
-        id:"",
-        todo:""
+      id: "",
+      todo: "",
+      date:""
     });
-   };
-   
+  };
 
-   const deleteHandler=(id)=>{
-    const newTodoList=todoList.filter((item)=> item.id !== id);
-    setTodoList(newTodoList)
-   };
+  const deleteHandler = (id) => {
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodoList);
+  };
 
-   const editHandler=(id,newTodo)=>{
-    const newTodoListAfterEdit=todoList.map((item)=> item.id === id ? {...item,todo:newTodo}:item);
-    setTodoList(newTodoListAfterEdit)
-   };
-
-
-
-
-    return (
-        <>
-            <div className="container max-w-7xl mx-auto h-screen">
-                <div className="bg-blue-500 sm:w-7/12 mx-auto sm:mt-5 rounded  shadow-sm shadow-amber-50">
-                  <h1 className="pt-4 sm:text-3xl font-bold text-center">Todo App</h1>
-                  <div className="h-[1px] bg-white mx-auto w-3/12 mt-2 "></div>
-                  <AddTodoInput todo={todo.todo} tHandler={todoHandler} addtodoHandler={addtodoHandler}/>
-                  <TodoList list={todoList} deleteHandler={deleteHandler} editHandler={editHandler}/>
-                </div>
-               {modal && <Modal  setModal={setModal}/>}
-                
-            </div>
-        </>
+  const editHandler = (id, newTodo) => {
+    const newTodoListAfterEdit = todoList.map((item) =>
+      item.id === id ? { ...item, todo: newTodo } : item
     );
-}
+    setTodoList(newTodoListAfterEdit);
+  };
+
+  return (
+    <>
+      <div className="container max-w-7xl mx-auto h-screen">
+        <div className="bg-blue-500 sm:w-7/12 mx-auto sm:mt-5 rounded  shadow-sm shadow-amber-50">
+          <h1 className="pt-4 sm:text-3xl font-bold text-center">Todo App</h1>
+          <div className="h-[1px] bg-white mx-auto w-3/12 mt-2 "></div>
+          <AddTodoInput
+            todo={todo.todo}
+            tHandler={todoHandler}
+            addtodoHandler={addtodoHandler}
+          />
+          <TodoList
+            list={todoList}
+            deleteHandler={deleteHandler}
+            editHandler={editHandler}
+          />
+        </div>
+        {modal && <Modal setModal={setModal} />}
+      </div>
+    </>
+  );
+};
 
 export default Todo;
