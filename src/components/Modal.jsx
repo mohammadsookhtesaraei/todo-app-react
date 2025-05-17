ad2cdbd6effcb9ffab3d11650ceec40f3f8751cc
mohammadso <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-05-17 19:59:26 +0330

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ import { RxCross2 } from "react-icons/rx";
 
 const Modal = ({ setModal }) => {
   return (
-    <div className="fixed top-0 left-0 w-full h-screen backdrop-blur-xs">
+    <div className="fixed top-0 left-0 w-full h-full backdrop-blur-xs">
       <div className="container mx-auto max-w-80 mt-30 rounded border-1 bg-[#242424] h-45 flex flex-col items-end shadow-sm shadow-white">
         <button
           className="m-2 cursor-pointer hover:scale-90 transition duration-150 ease-in-out"
